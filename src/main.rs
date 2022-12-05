31d4f5335de5dc8383b38ee2bec38fd2ad138cd6
Adam Shell <adam@granseal.net>
2022-12-05 08:14:37 -0500

    No more "fancy" profiler struct. Not worth it.

--- src/main.rs
+++ src/main.rs
@@ -33,50 +33,18 @@ fn main() {
         println!("No input for day {} found.",day);
     }
 }
-mod util {
-    use std::time::Instant;
-    use colored::Colorize;
 
-    pub(crate) struct Profiler {
-        name: String,
-        timer: Instant,
-        global: Instant,
-    }
-    impl Profiler {
-        pub fn new(title: &str) -> Self {
-            Self {
-                name: String::from(title),
-                timer: Instant::now(),
-                global: Instant::now(),
-            }
-        }
-        pub fn log(&mut self, msg: &str) {
-            let elapsed = self.timer.elapsed();
-            self.timer = Instant::now();
-            println!("{} - {}",msg.bright_green(),format!("{:?}",elapsed).yellow());
-        }
-        pub fn total(&self) {
-            println!("{} - {} - {}",self.name.bright_green(),"total".yellow(),format!("{:?}",self.global.elapsed()).yellow());
-        }
-    }
-}
 mod day1 {
     use std::str::FromStr;
-    use crate::util::Profiler;
+    use std::time::Instant;
 
     pub fn day1(data: String) {
-        let mut prof = Profiler::new("Day 1, Part 1");
-        prof.log("Read File");
+        let timer = Instant::now();
         let input = data.lines().collect();
-        prof.log("Get Lines");
         let elves = construct_elves(&input);
-        prof.log("Construct Elves");
-        let totals = calculate_elf_totals(&elves);
-        prof.log("Calculate Calories");
-        let top_three = &totals.as_slice()[0..=2].iter().fold(0,|acc,(_,c)| acc + c);
-        prof.log("Aquire Top Three");
-        prof.total();
-        println!("Most calories: {:?} from elf {}",totals.first().expect("Elf").1,totals.first().expect("Elf").0);
+        let top_three = &elves.as_slice()[0..=2].iter().fold(0,|acc,e| acc + e.total);
+        println!("Completed in {:?}",timer.elapsed());
+        println!("Most calories: {:?}",elves.first().expect("Elf").total);
         println!("Top three combined calories: {:?}",top_three);
         println!("{:?} Elves",elves.len());
         println!("{:?} food items.",&elves.iter().fold(0,|acc, items|{acc + items.food.len()}));
@@ -84,41 +52,33 @@ mod day1 {
 
     pub fn construct_elves(lines: &Vec<&str>) -> Vec<Elf> {
         let mut elves = vec![];
-        let mut elf = Elf { food: vec![] };
+        let mut elf = Elf { food: vec![], total: 0 };
         for food in lines {
             if !food.is_empty() {
                 if let Ok(calories) = i32::from_str(food) {
                     elf.food.push(calories);
+                    elf.total += calories;
                 } else { println!("Invalid input") }
             } else {
                 elves.push(elf);
-                elf = Elf { food: vec![] };
+                elf = Elf { food: vec![], total: 0 };
             }
         }
         elves.push(elf);
+        elves.sort_by(|a,b| b.total.cmp(&a.total));
         elves
     }
 
     #[derive(Debug)]
     pub struct Elf {
-        pub(crate) food: Vec<i32>
-    }
-
-    pub fn calculate_elf_totals(elves: &Vec<Elf>) -> Vec<(usize, i32)> {
-        let mut totals = vec![];
-        for (i, elf) in elves.iter().enumerate() {
-            let total = elf.food.iter().fold(0, |acc, x| acc + x);
-            totals.push((i, total));
-        }
-        totals.sort_by(|(_, a), (_, b)| b.cmp(a));
-        totals
+        pub food: Vec<i32>,
+        pub total: i32,
     }
 }
 mod day2 {
-    use rayon::prelude::*;
+    use std::time::Instant;
     use RPS::*;
     use WLD::*;
-    use crate::util::Profiler;
 
     #[derive(Debug,Copy,Clone)]
     pub enum RPS {
@@ -135,32 +95,28 @@ mod day2 {
         Unknown,
     }
     pub fn day2(data: String) {
-        let mut p = Profiler::new("Day 2 Part 1");
+        let timer = Instant::now();
         // part 1.
-        let input = data.par_split('\n')
-            .map(|s| s.par_split(' ').collect::<Vec<_>>())
+        let input = data.split('\n')
+            .map(|s| s.split(' ').collect::<Vec<_>>())
             .map(|s| (*s.first().unwrap(),*s.last().unwrap()))
             .map(|(them,you)| (map_data_rps(them),map_data_rps(you)))
             .collect::<Vec<_>>();
-        p.log("Input Processed");
         let score = input.iter().fold(0,|acc,(them,you)|{acc + score_round(them,you)});
-        p.log(format!("Played {} rounds of RPS",input.len()).as_str());
+        println!("Completed in {:?}",timer.elapsed());
         println!("Score: {:?}",score);
-        p.total();
 
         // part 2.
-        let mut p = Profiler::new("Day 2 Part 2");
-        let input = data.par_split('\n')
+        let timer = Instant::now();
+        let input = data.split('\n')
             .map(|s| s.split(" ").collect::<Vec<_>>())
             .map(|s| (*s.first().unwrap(),*s.last().unwrap()))
             .map(|(them,you)| (map_data_rps(them),map_win_lose_draw(you)))
             .map(|(them, you)| (them, play_to_wld(&them,&you)))
             .collect::<Vec<_>>();
-        p.log("Processed Input");
         let score = input.iter().fold(0,|acc,(them,you)|{acc + score_round(them,you)});
-        p.log(format!("Playing {} rounds of RPS",input.len()).as_str());
+        println!("Completed in {:?}",timer.elapsed());
         println!("Score: {:?}",score);
-        p.total();
     }
     pub fn map_win_lose_draw(value: &str) -> WLD {
         match value {
@@ -208,7 +164,7 @@ mod day2 {
     }
 }
 mod day3 {
-    use crate::util::Profiler;
+    use std::time::Instant;
 
     #[derive(Debug)]
     pub struct Rucksack {
@@ -216,23 +172,22 @@ mod day3 {
         b: String,
     }
     pub fn day3(data: String) {
-        let mut p = Profiler::new("Day 3 Part 1");
+        let timer = Instant::now();
         let input = data.lines()
             .map(|s| Rucksack {
                 a: s[0..s.len() / 2].to_owned(),
                 b: s[s.len() / 2..s.len()].to_owned(),
             }).collect::<Vec<_>>();
-        p.log("Create Rucksack Vector from input");
         // part 1
         let sum = input.iter().map(|r| get_common_item(&r))
             .map(|i| score_item(&(i as u8)))
             .sum::<i32>();
-        p.log("Find common item and score and sum.");
+        println!("Completed in {:?}",timer.elapsed());
         println!("{:?}",sum);
-        p.total();
-        p = Profiler::new("Day 3 Part 2");
+
+        let timer = Instant::now();
         let elf_groups = get_elf_badges(&input);
-        p.total();
+        println!("Completed in {:?}",timer.elapsed());
         println!("{:?}",elf_groups);
     }
     pub fn get_elf_badges(elves: &Vec<Rucksack>) -> i32 {
@@ -280,44 +235,12 @@ mod day3 {
     }
 }
 mod day4 {
-    use std::ops::Range;
     use std::str::FromStr;
+    use std::time::Instant;
 
     pub fn day4(data: String) {
-        let contained = |r: Vec<Vec<i32>>| {
-            if (r[0][0] >= r[1][0] &&
-                r[0][1] <= r[1][1]) ||
-                (r[1][0] >= r[0][0] &&
-                    r[1][1] <= r[0][1])
-            {true} else {false}
-        };
-        let result = data
-            .lines()
-            .map(|l| l.split(',')
-            .collect::<Vec<_>>())
-            .map(|r| {
-                r.iter()
-                    .map(|v| v.split('-')
-                        .map(|n| i32::from_str(n).unwrap_or(0))
-                        .collect::<Vec<_>>())
-                    .collect::<Vec<_>>()
-            })
-            .map(|r| contained(r))
-            .filter(|v| *v == true )
-            .collect::<Vec<_>>()
-            .len();
-
-        println!("{:?}",result);
-
-        let overlap = |r: Vec<Vec<i32>>| {
-            if (r[0][1] >= r[1][0] &&
-                r[0][0] <= r[1][1]) ||
-               (r[1][1] >= r[0][0] &&
-                r[1][0] <= r[0][1])
-            {true} else {false}
-        };
-        let result = data
-            .lines()
+        pub fn solver(data: &str, cmp: impl Fn(Vec<Vec<i32>>) -> bool) -> usize {
+            data.lines()
             .map(|l| l.split(',')
                 .collect::<Vec<_>>())
             .map(|r| {
@@ -327,11 +250,32 @@ mod day4 {
                         .collect::<Vec<_>>())
                     .collect::<Vec<_>>()
             })
-            .map(|r| overlap(r))
+            .map(|r| cmp(r))
             .filter(|v| *v == true )
             .collect::<Vec<_>>()
-            .len();
-
-        println!("{:?}",result);
+            .len()
+        }
+        let contained = |r: Vec<Vec<i32>>| {
+            if (r[0][0] >= r[1][0] &&
+                r[0][1] <= r[1][1]) ||
+               (r[1][0] >= r[0][0] &&
+                r[1][1] <= r[0][1])
+            {true} else {false}
+        };
+        let timer = Instant::now();
+        let result = solver(data.as_str(),contained);
+        println!("Completed in {:?}",timer.elapsed());
+        println!("{}",result);
+        let overlap = |r: Vec<Vec<i32>>| {
+            if (r[0][1] >= r[1][0] &&
+            r[0][0] <= r[1][1]) ||
+            (r[1][1] >= r[0][0] &&
+            r[1][0] <= r[0][1])
+            {true} else {false}
+        };
+        let timer = Instant::now();
+        let result = solver(data.as_str(),overlap);
+        println!("Completed in {:?}",timer.elapsed());
+        println!("{}",result);
     }
 }
